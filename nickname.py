773d2a04e43b716268f702465bf44e92684e6fd5
mrsupernatural <157751452+mrsupernatural@users.noreply.github.com>
2024-03-18 18:41:17 +0300

    Add files via upload

---
+++ nickname.py
@@ -0,0 +1,5 @@
+#nickname name generator
+print("Welcome To Chatbot_Er_ryun")
+city=input("what is the city born in?")
+pet=input("what's your favorite pet name?")
+print("your nick_name could be "+city+" "+pet+"s")
